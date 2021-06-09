9f4a5faf4e71597c305324f8322672bfeb511d9c
jasper <jasperzhou1129@gmail.com>
2021-06-08 22:27:07 -0400

    javaDoc

---
+++ src/Javadoc.java
@@ -0,0 +1,9 @@
+public class Javadoc {
+    /**
+     * Multiple lines of Javadoc text are written here
+     * wrapped normally
+     */
+    public int method(String p1) { ... }
+
+    /** An especially short bit of Javadoc. */
+}
